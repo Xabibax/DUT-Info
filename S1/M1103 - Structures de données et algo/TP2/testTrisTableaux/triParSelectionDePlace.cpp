bfa5450f47dca84b1fbf929404c9ea70b33ee3eb
valoufr33380 <valougraglia@gmail.com>
2015-12-02 07:38:24 +0100

    Bug fixed

--- S1/M1103 - Structures de données et algo/TP2/testTrisTableaux/triParSelectionDePlace.cpp
+++ S1/M1103 - Structures de données et algo/TP2/testTrisTableaux/triParSelectionDePlace.cpp
@@ -1,7 +1,34 @@
 #include "triParSelectionDePlace.h"
 #include "echanger.h"
 
+#include <iostream>
+using namespace std;
+
 void triParSelectionDePlace(int tab[], unsigned short int nbCases)
 {
+    int position = nbCases-1;
+    int maximum = tab[0];
+    int save;
+    int positionMaximum;
 
+    while(!(position == 0))
+    {
+        maximum = tab[0];
+
+        // Déterminer maximum*/
+        for(int i=0; i<position; i++)
+        {
+            if(maximum < tab[i])
+            {
+                maximum = tab[i];
+                positionMaximum = i;
+            }
+        }
+
+        save = tab[position];
+        tab[position] = maximum;
+        tab[positionMaximum] = save;
+
+        position--;
+   }
 }
