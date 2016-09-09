b482f7dda0d41f488e5d96e4640c1f1e70576cfa
valoufr33380 <valougraglia@gmail.com>
2016-09-09 21:08:46 +0200

    Add 'S3' folder & uploaded first modules

---
+++ S3/M3103-Algorithmique_Avancée/TP1/src/TP1.java
@@ -0,0 +1,59 @@
+
+public class TP1 {
+
+	public static void main(String[] args)
+	{
+		TP1 t = new TP1();
+				
+		int tab[] = {7,2,4,0,2,0,8,1,-2,10};
+		int choix = 3;
+		
+		switch(choix)
+		{
+		case 1:
+			t.affichageDuTableau(tab);
+			break;
+		case 2:
+			t.afficherSommeTableau(tab);
+			break;
+		case 3:
+			t.trierValeursTableau(tab);
+			t.affichageDuTableau(tab);
+			break;
+		}
+	}
+	
+	void affichageDuTableau(int tab[])
+	{
+		for(int i=0; i<tab.length; i++)
+			System.out.println(tab[i]);
+	}
+	
+	int afficherSommeTableau(int tab[])
+	{
+		int somme = 0;
+		for(int i=0; i<tab.length; i++)
+			somme+=tab[i];
+		
+		System.out.println(somme);
+		return 0;
+	}
+	
+	void trierValeursTableau(int tab[])
+	{
+		int temp;
+		
+		for(int i=0; i<tab.length; i++)
+		{
+			for(int j=0; j<tab.length - 1; j++)
+			{
+				if(tab[j] > tab[j+1])
+				{
+					temp = tab[j+1];
+					tab[j+1] = tab[j];
+					tab[j] = temp;
+				}
+			}
+		}
+	}
+}
